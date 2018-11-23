f77b5397393ef7f9b95b45381640e8f291efcda5
kimtaesu <kimtaesoo188@gmail.com>
2018-11-24 03:00:20 +0900

    fix lint

--- app/src/main/java/com/hucet/github/MainActivity.kt
+++ app/src/main/java/com/hucet/github/MainActivity.kt
@@ -2,7 +2,6 @@ package com.hucet.github
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.hucet.github.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
